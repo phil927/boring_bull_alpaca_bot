ed8205178b56e4ede1dc670588caa1b64a6abbac
Phillip Stephens <pstephens9275@gmail.com>
2021-08-24 16:58:30 -0400

    Finished prototype, now need to test! And add logging/text alerts

--- alpaca_test.py
+++ alpaca_test.py
@@ -6,11 +6,18 @@ import alpaca_trade_api as tradeapi
 import urllib.request
 import json
 import re
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import calendar
+from time import sleep
 
 stock_to_purchase = "SPXL"
 boring_bull_json_url = "http://boringbull.com/assets/js/excel.js"
+# 24-hour Format for Trading Time in EST
+trading_hour = 15
+trading_minute = 30
+# Bot will execute trade on X mins either side of trading time
+acceptable_trading_window = 5
+
 
 api = tradeapi.REST(
     key_id="PKP02C3QC742LGWNPCBO",
@@ -63,8 +70,6 @@ def all_out():
 
 
 # Returns an int list of form [days_in, days_out]
-
-
 def get_days_in_and_out():
     hardcode_data = "var fldDaysIn = 5; var fldDaysOut = 0;"
     data = ''
@@ -99,53 +104,58 @@ def make_trade(trade_action: str):
         alert("Holding: No trade required today")
     elif trade_action == "buy":
         alert("Buying Stock Today!")
+        all_in()
     else:
         alert("Selling Stock Today")
+        all_out()
 
 
-def is_time_to_trade() -> bool:
-    trading_hour = 15
-    trading_minute = 30
-
-    UTC = pytz.utc
-    timeZ_Ea = pytz.timezone('US/Eastern')
-    print(timeZ_Ea)
-    time_in_us_eastern = timeZ_Ea.localize(datetime.now())
-    print(time_in_us_eastern)
-
-    trading_time = datetime(
-        time_in_us_eastern.year, time_in_us_eastern.month, time_in_us_eastern.day, trading_hour, trading_minute, tzinfo=timeZ_Ea)
-    print(trading_time)
-
-    time_until_trading = abs(trading_time - time_in_us_eastern)
-    print(f"Time until Trading: {time_until_trading}")
-
-
-# BUG IN CALCULATING the time until trading
-
-    print(date.today(tzinfo=timeZ_Ea).year())
-
-    trading_time = datetime(time_in_us_eastern.year(), time_in_us_eastern.month(
-    ), time_in_us_eastern.day(), hour=15, minute=30, second=0, microsecond=0, tzinfo=timeZ_Ea)
-
-    print(time_in_us_eastern)
-    print(time_in_us_eastern.strftime('%Y-%m-%d %H:%M:%S %Z %z'))
-    print(time_in_us_eastern.strftime('-%d %H:%M:%S %Z %z'))
-    day_of_the_week_index = time_in_us_eastern.weekday()
-    day_of_the_week_index = 4
+def is_trading_day(current_time_localized: datetime) -> bool:
+    day_of_the_week_index = current_time_localized.weekday()
     day_of_the_week = calendar.day_name[day_of_the_week_index]
     print(f"{day_of_the_week} with index: {day_of_the_week_index:d}")
 
     if day_of_the_week_index >= 5:
         alert("It's the weekend, no trades possible")
         return False
+    else:
+        alert("It is a trading day.")
+        return True
 
-    # get_days_in_and_out()
-# decide_to_buy_sell_or_hold(5, 0)
-# days_list = get_days_in_and_out()
-# trade_action = decide_to_buy_sell_or_hold(
-#     days_in=days_list[0], days_out=days_list[1])
-# make_trade(trade_action)
-# is_time_to_trade()
-all_out()
-# all_in()
+
+def is_time_to_trade() -> bool:
+    UTC = pytz.utc
+    timeZ_Ea = pytz.timezone('US/Eastern')
+    time_in_us_eastern = timeZ_Ea.localize(datetime.now())
+    print(time_in_us_eastern)
+
+    if not is_trading_day(time_in_us_eastern):
+        return False
+    else:
+        trading_time = datetime(
+            time_in_us_eastern.year, time_in_us_eastern.month, time_in_us_eastern.day, trading_hour, trading_minute)
+        trading_time = timeZ_Ea.localize(trading_time)
+        time_until_trading = trading_time - time_in_us_eastern
+
+        if abs(time_until_trading) < timedelta(minutes=acceptable_trading_window):
+            alert("It's trading time!")
+            return True
+        else:
+            alert(
+                f"Not trading time: \nCurrent Time: {time_in_us_eastern}\nTrading Time: {trading_time}")
+            return False
+
+
+if __name__ == "__main__":
+    running_continuosly = True
+
+    while True:
+        if is_time_to_trade():
+            days_in_out_list = get_days_in_and_out()
+            trade_decision = decide_to_buy_sell_or_hold(
+                days_in_out_list[0], days_in_out_list[1])
+            make_trade(trade_decision)
+        if not running_continuosly:
+            break
+        # Sleeps for 5 minutes
+        sleep(5*60)
